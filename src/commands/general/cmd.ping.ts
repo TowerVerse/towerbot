66da315ac075cee0ce20366d9a56dc4493e9c1b9
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2021-06-25 15:22:13 +0530

    update header

--- src/commands/general/cmd.ping.ts
+++ src/commands/general/cmd.ping.ts
@@ -1,3 +1,13 @@
+// License: GPL-3
+// Maintainer: Otterlord#3653, KittyBorgX#1415
+// Contributors:
+//
+// File description:
+//     The file for the !ping command of the TowerBot
+//
+// Extra info:
+//     Displays the latency between you and the Discord API
+//
 
 import { Message } from "discord.js";
 import { App } from "../../app";
@@ -5,18 +15,16 @@ import { Command } from "../../structures/Command";
 import { MessageEmbed } from "discord.js";
 
 const command = new Command({
-  name: 'ping',
-  desc: 'Ping the bot'
-})
+  name: "ping",
+  desc: "Ping the bot",
+});
 
 command.setExecutor(async (app: App, message: Message) => {
   const embed = new MessageEmbed()
     .setTitle("Pong!")
-    .setDescription(
-      `⏲️ Ping: ${Math.round(message.client.ws.ping)} ms`
-    )
-    .setColor('#fb644c');
-  message.channel.send(embed)
-})
+    .setDescription(`⏲️ Ping: ${Math.round(message.client.ws.ping)} ms`)
+    .setColor("#fb644c");
+  message.channel.send(embed);
+});
 
-export default command
+export default command;
