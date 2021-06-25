a65aea5c5dc002d52ab2f70392e9a4159070432d
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2021-06-25 08:58:54 +0530

    Add latency ping command and update .gitignore

--- src/commands/cmd.ping.ts
+++ src/commands/cmd.ping.ts
@@ -1,14 +1,29 @@
 import { Message } from "discord.js";
 import { App } from "../app";
 import { Command } from "../structures/Command";
+import { Client } from "discord.js" 
+import { MessageEmbed } from "discord.js";
+
+const client = new Client()
 
 const command = new Command({
   name: 'ping',
   desc: 'Ping the bot'
 })
 
-command.setExecutor((app: App, msg: Message) => {
-  msg.reply('Pong!')
+command.setExecutor(async (app: App, message: Message) => {
+  const msg = await message.channel.send("Pinging...");
+  const Embed = new MessageEmbed()
+    .setTitle("Pong!")
+    .setAuthor(`${message.author.username}` , message.author.displayAvatarURL())
+    .setDescription(
+      `⌛ Latency is ${Math.floor(
+        msg.createdTimestamp - message.createdTimestamp
+      )}ms\n⏲️ API Ping is ${Math.round(message.client.ws.ping)}`
+    )
+    .setColor('#fb644c');
+  msg.edit(Embed);
+
 })
 
 export default command
